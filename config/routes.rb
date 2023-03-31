b3886bff546d7a930edc299a2243d53309dd3b44
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-03-31 23:17:54 +0100

    create api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'greetings/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "greetings#index"
+  resources :greetings, only: [:index]
 end
